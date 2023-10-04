a79c747617ed69f5052998832f2a236228a1c669
Lzynee <145524819+Lzynee@users.noreply.github.com>
2023-10-04 16:57:54 +0900

    논리연산 실습

---
+++ java_train/week_3/BinaryOne.java
@@ -0,0 +1,9 @@
+package com.mystudy.binary;
+
+public class BinaryOne {
+    public static void main(String[] args) {
+        int i = 5, j = 7;
+        System.out.println(i&j);  // 이진수의 논리연산곱
+        System.out.println((i<7)&&(j>5));  // 논리연산곱
+    }
+}
