6d748e3e223c302bd0fdda2c7ea857a19d7616ac
Lzynee <145524819+Lzynee@users.noreply.github.com>
2023-10-04 14:14:08 +0900

    자바 연습 - 구구단 3단 출력하기

---
+++ java_train/week_3/test100423_2.java
@@ -0,0 +1,13 @@
+/**
+ *  구구단 3단을 출력하는 프로그램
+ * */
+
+package com.mystudy.etc;
+
+public class test100423_2 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 9; i++) {
+            System.out.printf("3 * %d = %2d%n", i, 3 * i);
+        }
+    }
+}
